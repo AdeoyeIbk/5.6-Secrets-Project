1ded70e85413cf918b897695b0673aae73005521
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2024-11-01 14:30:14 +0100

    Comment out previous route handler for index.ejs and prepare for async(AXIOS) implementation

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const port = 5000;
 // 3. Use the public folder for static files.
 app.use(express.static('public'));
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get('/', (req, res) => {
-  res.render('index.ejs', {secret: data, user: username});
-})
+// app.get('/', (req, res) => {
+//   res.render('index.ejs', {secret: data, user: username});
+// })
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
 app.get('/', async (req, res) => {
