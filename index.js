9370b0189b0ff7e8c78992ae556dc80d843abca9
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2024-11-01 13:40:25 +0100

    Add route to render index.ejs with secret and username which will be passed to the index.ejs

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const port = 5000;
 // 3. Use the public folder for static files.
 app.use(express.static('public'));
 // 4. When the user goes to the home page it should render the index.ejs file.
-
+app.get('/', (req, res) => {
+  res.render('index.ejs', {secret: data, user: username});
+})
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
+
 
 // 6. Listen on your predefined port and start the server.
